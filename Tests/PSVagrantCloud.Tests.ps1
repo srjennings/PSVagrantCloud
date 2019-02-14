ccf8061fa760fd5ff4aeaff437336970b5db5127
Steven Jennings <steven@automatingops.com>
2019-02-14 16:24:19 -0500

    adds scriptanalyzer test, remove empty line

--- Tests/PSVagrantCloud.Tests.ps1
+++ Tests/PSVagrantCloud.Tests.ps1
@@ -2,12 +2,12 @@ $projectRoot = Resolve-Path "$PSScriptRoot\.."
 $moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psm1")
 $moduleName = Split-Path $moduleRoot -Leaf
 
+$scripts = Get-ChildItem $projectRoot -Include *.ps1,*.psm1,*.psd1 -Recurse
+
 Describe "General project validation: $moduleName" {
 
-    $scripts = Get-ChildItem $projectRoot -Include *.ps1,*.psm1,*.psd1 -Recurse
-
     # TestCases are splatted to the script so we need hashtables
-    $testCase = $scripts | Foreach-Object{@{file=$_}}         
+    $testCase = $scripts | Foreach-Object{@{file=$_}}
     It "Script <file> should be valid powershell" -TestCases $testCase {
         param($file)
 
@@ -21,5 +21,28 @@ Describe "General project validation: $moduleName" {
 
     It "Module '$moduleName' can import cleanly" {
         {Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force } | Should Not Throw
+    }
+}
+
+Describe "PSScriptAnalyzer for module: $Script:ModuleName" {
+    $rules = Get-ScriptAnalyzerRule -Severity Error
+
+    foreach ($file in $scripts) {
+        Context "Script '$($file.FullName)'" {
+            $results = Invoke-ScriptAnalyzer -Path $file.FullName -IncludeRule $rules
+            if ($results) {
+                foreach ($rule in $results) {
+                    It $rule.RuleName {
+                        $message = "{0} Line {1}: {2}" -f $rule.Severity, $rule.Line, $rule.message
+                        $message | Should -Be ""
+                    }
+                }
+            }
+            else {
+                It "PSScriptAnalyzer Results PASSED" {
+                    $results | Should -BeNullOrEmpty
+                }
+            }
+        }
     }
 }
