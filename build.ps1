b886090f3f233d76d8ff62a329eb283353323b97
deyjcode <steven@automatingops.com>
2019-02-14 23:22:45 -0500

    faster clean step

--- build.ps1
+++ build.ps1
@@ -10,27 +10,38 @@ $global:ModuleVersionType = $ModuleVersion
 Write-Output "
 ** ---Build Task '$Task' Starting--- **
 "
-
-Write-Output "Installing Dependencies for Build"
-# Install dependencies
-Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-Install-Module InvokeBuild, PSDeploy, BuildHelpers, PSScriptAnalyzer, PlatyPS -force -Scope CurrentUser
-Install-Module Pester, PowerShellGet -Force -SkipPublisherCheck -Scope CurrentUser -AllowClobber
-
-Write-Output "Importing Dependencies for Build"
-# Import Modules
-Import-Module InvokeBuild, BuildHelpers, PSScriptAnalyzer
-
-Write-Output "Setting Build Parameters"
-Set-BuildEnvironment -Force
-
-Write-Output "Beginning Invoke-Build Sequence"
-# Invoke-Build will now start looking to the module build file for tasks
-Invoke-Build $Task -Result Result
+if ($Task -eq 'Clean') {
+    Invoke-Build $Task -Result Result
 
 if ($Result.Error){
     exit 1
 }
 else {
     exit 0
+}
+}
+else {
+    Write-Output "Installing Dependencies for Build"
+    # Install dependencies
+    Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+    Install-Module InvokeBuild, PSDeploy, BuildHelpers, PSScriptAnalyzer, PlatyPS -force -Scope CurrentUser
+    Install-Module Pester, PowerShellGet -Force -SkipPublisherCheck -Scope CurrentUser -AllowClobber
+    
+    Write-Output "Importing Dependencies for Build"
+    # Import Modules
+    Import-Module InvokeBuild, BuildHelpers, PSScriptAnalyzer
+    
+    Write-Output "Setting Build Parameters"
+    Set-BuildEnvironment -Force
+    
+    Write-Output "Beginning Invoke-Build Sequence"
+    # Invoke-Build will now start looking to the module build file for tasks
+    Invoke-Build $Task -Result Result
+    
+    if ($Result.Error){
+        exit 1
+    }
+    else {
+        exit 0
+    }
 }
